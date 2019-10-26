d9c0358e44eae3eb5d77fb9b90801ceed41d4b9a
shin-elab <aton.kish.elab@gmail.com>
2019-10-26 22:34:30 +0900

    Fix: fixed typos

--- sensor/server.py
+++ sensor/server.py
@@ -14,7 +14,7 @@ RECV_SIZE = 1024
 WF_ADDR = 'b8:27:eb:a9:5e:97'
 
 PIN = 21
-TIMEOUT = 3000 # mill sec
+TIMEOUT = 3000 # milli sec
 
 URL = 'http://ec2-13-115-229-32.ap-northeast-1.compute.amazonaws.com'
 
@@ -44,7 +44,7 @@ def connect_with_neck():
                     if calibration_flag:
                         offset_p = p_neck - p_waist
                         calibration_flag = False
-                        print(f'neck:{p_neck},waist:{p_waist},offset:{offset_p}')
+                        print(f'Neck: {p_neck}, Waist: {p_waist}, Offset:{offset_p}')
                     p_waist += offset_p
 
                     if bow_flag:
