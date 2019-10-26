407d5520550242548350c75de8c6bf1f758dd913
EC2 Default User <ec2-user@ip-172-31-44-102.ap-northeast-1.compute.internal>
2019-10-26 11:28:42 +0000

    正規化できた

--- server/normalize.py
+++ server/normalize.py
@@ -1,27 +1,49 @@
+import csv
+import math
+
 SeaLevelPressure = 900
 
 def pressure_to_height(pressure):
     height = ((pressure/SeaLevelPressure)**(1/5.275)-1)*(15+273.15)/0.0065
     return height
 
-def height_to_angle(height1, height2):
-    angle = math.asin(height1-height2)
+def height_to_angle(diff_height):
+    angle = math.asin(diff_height)
     return angle
 
 def normalize(csv_file):
     with open(csv_file, 'r') as f:
         reader = csv.reader(f)
+        height_list = []
         angle_list = []
         normalized_list = []
+        time_list = []
         for raw in reader:
 #            angle_list.append(height_to_angle(pressure_to_height(float(raw[1])), pressure_to_height(float(raw[2]))))
-            angle_list.append(height_to_angle(float(raw[1])) - pressure_to_height(float(raw[2])))
-        max_length = max(angle_list)
-        for angle in angle_list:
-            angle = height_to_angle(angle/max_length)
-        max_angle = max(angle_list)
+            height_list.append(pressure_to_height(float(raw[2])) - pressure_to_height(float(raw[1])))
+            time_list.append(raw[0])
+        print(height_list)
+        max_length = max(height_list)
+        for height in height_list:
+            angle_list.append(height_to_angle(height/max_length))
+#        print(angle)
+        print(angle_list)
+        max_angle = math.pi/2
         min_angle = min(angle_list)
         for index, angle in enumerate(angle_list):
-            normalized_list.append([reader[index][0], str((angle-min_angle)/(max_angle-min_angle))])
-        print(normalized_list)
+            if angle <= 90-min_angle:
+                normalized_list.append([time_list[index], str((angle-min_angle)/(max_angle-min_angle))])
+            else:
+                normalized_list.append([time_list[index], str(1)])
+
+    with open('normalized_data/' + csv_file.split('/')[1], 'w') as wf:
+        writer = csv.writer(wf)
+        for normalized_data in normalized_list:
+            writer.writerow(normalized_data)
     return normalized_list
+
+def main():
+    return
+
+if __name__ == '__main__':
+    main()
